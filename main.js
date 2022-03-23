98f55791133c5a3d78a8673bd4668de34c08d10c
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2022-03-23 09:28:24 +0530

    Mine functionality removed bugs

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ class Block {
     this.nonce = 0;
     this.valid = true;
     this.hash = this.calculateHash();
+    this.newHash = this.hash;
   }
 
   calculateHash() {
@@ -26,14 +27,13 @@ class Block {
 
   mineBlock(difficulty) {
     // this.nonce = 0;
-    console.log(this.data, this.hash);
     while (
       this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
     ) {
       this.nonce++;
       this.hash = this.calculateHash();
     }
-    console.log(this.data, this.hash);
+    this.newHash = this.hash;
     return this.hash;
   }
 }
@@ -42,7 +42,7 @@ class Blockchain {
   constructor() {
     this.difficulty = 2;
     this.chain = [this.createGenenisBlock()];
-    this.displayBlocks(this.chain);
+    this.displayBlocks();
   }
 
   createGenenisBlock() {
@@ -60,8 +60,7 @@ class Blockchain {
   }
 
   addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    console.log(newBlock);
+    newBlock.previousHash = this.getLatestBlock().newHash;
     newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
     this.isChainValid();
@@ -74,7 +73,7 @@ class Blockchain {
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i - 1];
       if (
-        currentBlock.hash !== currentBlock.calculateHash() ||
+        currentBlock.hash !== currentBlock.newHash ||
         currentBlock.previousHash !== previousBlock.hash ||
         previousBlock.valid === false
       ) {
@@ -88,6 +87,13 @@ class Blockchain {
     if (i !== this.chain.length) {
       while (i < this.chain.length) {
         this.chain[i].valid = false;
+        console.log(
+          this.chain[i].previousHash,
+          this.chain[i].hash,
+          this.chain[i].newHash
+        );
+        this.chain[i].previousHash = this.chain[i - 1].newHash;
+        this.chain[i].newHash = this.chain[i].calculateHash();
         i++;
       }
       this.displayBlocks();
@@ -107,13 +113,13 @@ class Blockchain {
     <div data-valid=${block.valid} data-index=${
         block.index
       } class="eachBlock rounded-3xl p-4 m-8 grid grid-rows-4 gap-4 shadow-lg ${
-        block.valid
+        block.hash === block.newHash
           ? "border-2 border-green-400 bg-[#fcfffd] shadow-green-400"
           : "border-2 border-rose-500 bg-rose-100 shadow-rose-500"
       }">
         <div class="details-data border border-[#d9d9d9] rounded overflow-hidden">
           <p class="${
-            block.valid ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
+            block.hash === block.newHash ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
           } text-[#575757] font-medium p-2 border-r border-[#d9d9d9]">DATA</p>
           <input
             class="p-2 -ml-1 w-11/12 text-[#7d7d7d] font-semibold inline-block data-input bg-transparent ${
@@ -132,7 +138,7 @@ class Blockchain {
           class="details-previousHash border border-[#d9d9d9] rounded overflow-hidden"
         >
           <p class=" ${
-            block.valid ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
+            block.hash === block.newHash ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
           } text-[#575757] font-medium p-2 border-r border-[#d9d9d9]">PREVIOUS HASH</p>
           <p class="p-2 text-[#5eb8ff] font-semibold previousHash -ml-1">${
             block.previousHash
@@ -140,18 +146,18 @@ class Blockchain {
         </div>
 
         <div class="details-hash border ${
-          block.valid ? "border-[#B7EB8F]" : "border-[#ffdce2]"
+          block.hash === block.newHash ? "border-[#B7EB8F]" : "border-[#ffdce2]"
         } rounded overflow-hidden ${
-        block.valid ? "bg-[#f6ffee]" : "bg-[#ffe2e7]"
+        block.hash === block.newHash ? "bg-[#f6ffee]" : "bg-[#ffe2e7]"
       }">
           <p class="text-[#575757] font-medium p-2 border-r border-[#d9d9d9">CURRENT HASH</p>
           <p class="p-2 hash ${
             block.hash == block.calculateHash()
               ? "text-[#44ab11]"
               : "text-[#f43f5e]"
-          } font-semibold -ml-1" id="hash" data-value="${
-        block.hash
-      }">${block.calculateHash()}</p>
+          } font-semibold -ml-1" id="hash" data-value="${block.hash}">${
+        block.newHash
+      }</p>
         </div>
 
         <div class="details-other flex justify-between">
@@ -159,7 +165,7 @@ class Blockchain {
             class="details-date border border-[#d9d9d9] rounded overflow-hidden"
           >
             <p class=" ${
-              block.valid ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
+              block.hash === block.newHash ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
             } text-[#575757] p-2 border-r border-[#d9d9d9]">DATE</p>
             <p class="p-2 date text-[#575757] font-normal">${
               block.timestamp
@@ -168,18 +174,18 @@ class Blockchain {
 
           <div
             class="details-nonce border border-[#d9d9d9] rounded overflow-hidden text-right inline-block ${
-              block.valid ? "inherit" : "hidden"
+              block.hash === block.newHash ? "inherit" : "hidden"
             }"
           >
             <p class=" ${
-              block.valid ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
+              block.hash === block.newHash ? "bg-[#fafafa]" : "bg-[#ffe2e7]"
             } text-[#575757] font-medium p-2 border-r border-[#d9d9d9]">NONCE</p>
             <p class="p-2 nonce text-[#575757] font-normal">${block.nonce}</p>
           </div>
 
           <div
             class="details-nonce border border-[#d9d9d9] rounded overflow-hidden text-right inline-block ${
-              block.valid ? "hidden" : "inherit"
+              block.hash === block.newHash ? "hidden" : "inherit"
             }""
           >
             <button
@@ -234,6 +240,7 @@ class Blockchain {
         const hash = this.querySelector("#hash").dataset.value;
         const block = thisInst.chain.find((block) => block.hash === hash);
         block.data = e.target.value;
+        block.newHash = block.calculateHash();
         isChainValidWithThis();
       });
     });
